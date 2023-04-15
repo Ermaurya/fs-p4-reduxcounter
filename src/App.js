e8b9f76cec7954784c8b8d967c05a4330867dfcb
shubham maurya <cs.shubhammaurya@gmail.com>
2023-04-16 00:09:55 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
   return (
     <div className="App">
       <h1>
-         Hello World <br /> A little Redux Project. YaaY!
+         Hello geekster <br /> this is a little Redux Project counter app
       </h1>
-      <h3>Counter</h3>
+    
       <h3>{counter}</h3>
       <button onClick={() => dispatch(increment())}>Increase</button>
       <button onClick={() => dispatch(reset())}>Reset</button>
